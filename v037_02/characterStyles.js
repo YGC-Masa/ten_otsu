b2f9370aba07d3de9a0ead828562257c07e05ca3
YGC-Masa <osaru355@gmail.com>
2025-08-15 14:51:43 +0900

    Update characterStyles.js

--- v037_02/characterStyles.js
+++ v037_02/characterStyles.js
@@ -5,5 +5,14 @@ window.characterStyles = {
   "翠": { fontSize: "1.2em", speed: 35, color: "#02b308" },
   "こがね": { fontSize: "1.2em", speed: 45, color: "#FFF450" },
   "琥珀": { fontSize: "1.3em", speed: 25, color: "#F68B1F" },
-  "店長": { fontSize: "1em", speed: 40, color: "#00ffff" }
+  "真花": { fontSize: "1.2em", speed: 40, color: "#C0C0C0" },
+  "雪乃": { fontSize: "1.2em", speed: 45, color: "##6495ED" },
+  "美空": { fontSize: "1.2em", speed: 40, color: "#fffef6" },
+  "夜空": { fontSize: "1.2em", speed: 35, color: "#00152d" },
+  "桃": { fontSize: "1.3em", speed: 35, color: "#F7ADC3" },
+  "彩愛": { fontSize: "1.2em", speed: 35, color: "#694D9F" },
+  "里美": { fontSize: "1.2em", speed: 45, color: "#8d5025" },
+  "萌": { fontSize: "1.1em", speed: 40, color: "#33CC99" },
+  "店長": { fontSize: "1.2em", speed: 40, color: "#00ffff" }
 };
+
