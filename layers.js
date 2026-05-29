cdb3535b339225ba5f79559110cf15f4ccb933c1
YGC-Masa <osaru355@gmail.com>
2026-05-29 11:18:11 +0900

    Add files via upload

---
+++ layers.js
@@ -0,0 +1,3 @@
+(function(){"use strict";window.TENOTSU_V039=window.TENOTSU_V039||{};const ns=window.TENOTSU_V039;ns.VERSION = "v039_52_story_bg_force_and_layer_order_fix";ns.state={mode:"boot",officeSelection:[],frontCharacter:null,bootedAt:Date.now()};ns.setMode=function(mode){ns.state.mode=mode;document.body.dataset.v039Mode=mode;["boot","office","story","shop","battle","members","settings","town"].forEach(m=>document.body.classList.toggle("v039-mode-"+m,mode===m));};})();
+
+(function(){ window.TENOTSU_V039.BUILD_LABEL = "v039_52"; })();
