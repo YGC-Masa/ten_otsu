1683aa6628ed5f41c3e9c32c4e18aa6b977accb9
YGC-Masa <osaru355@gmail.com>
2026-06-13 08:36:06 +0900

    Add files via upload

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,6 +1,6 @@
-/* v039_181 background landmark addition patch */
+/* v039_182 yozora affection lv3 step36-37 storyfile patch */
 (function(){
   "use strict";
-  window.TEN_OTSU_PATCH_VERSION = "v039_181";
-  window.TEN_OTSU_PATCH_LABEL = "背景追加：駅前あかり通り：夜 / 住宅街：夜";
+  window.TEN_OTSU_PATCH_VERSION = "v039_182";
+  window.TEN_OTSU_PATCH_LABEL = "夜空 親愛Lv.3「美空なら休憩室」ステップ36・37立ち絵修正";
 })();
