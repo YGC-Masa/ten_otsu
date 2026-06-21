6d8390464667a7841ab8c0c33d7e1759a16cc3a8
YGC-Masa <osaru355@gmail.com>
2026-06-21 11:07:16 +0900

    Add files via upload

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_246 */
+/* v039_247 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_246";
-  window.TENOTSU_BUILD_LABEL = "彩愛93 シナリオ調整";
-  window.TEN_OTSU_PATCH_VERSION = "v039_246";
-  window.TEN_OTSU_PATCH_LABEL = "彩愛93 シナリオ調整";
+  window.TENOTSU_BUILD_VERSION = "v039_247";
+  window.TENOTSU_BUILD_LABEL = "彩愛93 運転手・ばあや・背景調整";
+  window.TEN_OTSU_PATCH_VERSION = "v039_247";
+  window.TEN_OTSU_PATCH_LABEL = "彩愛93 運転手・ばあや・背景調整";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_246";
-  window.TENOTSU_V039.BUILD_LABEL = "彩愛93 シナリオ調整";
+  window.TENOTSU_V039.VERSION = "v039_247";
+  window.TENOTSU_V039.BUILD_LABEL = "彩愛93 運転手・ばあや・背景調整";
 })();
