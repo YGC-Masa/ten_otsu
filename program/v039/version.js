f9bb7cf3c39a602428b66e30887e8741e80d1960
YGC-Masa <osaru355@gmail.com>
2026-06-21 13:45:01 +0900

    Add files via upload

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_248 */
+/* v039_249 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_248";
+  window.TENOTSU_BUILD_VERSION = "v039_249";
   window.TENOTSU_BUILD_LABEL = "彩愛93 台詞・CG表示調整";
-  window.TEN_OTSU_PATCH_VERSION = "v039_248";
+  window.TEN_OTSU_PATCH_VERSION = "v039_249";
   window.TEN_OTSU_PATCH_LABEL = "彩愛93 台詞・CG表示調整";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_248";
+  window.TENOTSU_V039.VERSION = "v039_249";
   window.TENOTSU_V039.BUILD_LABEL = "彩愛93 台詞・CG表示調整";
 })();
