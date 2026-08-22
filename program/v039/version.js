e72177ee0ba8ae1feee184e3e9781f880e387d59
YGC-Masa <osaru355@gmail.com>
2026-08-22 18:15:00 +0900

    v039_281: update runtime version

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_280 */
+/* v039_281 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_280";
-  window.TENOTSU_BUILD_LABEL = "既存ブラックフェード流用・ストーリー開始整理";
-  window.TEN_OTSU_PATCH_VERSION = "v039_280";
-  window.TEN_OTSU_PATCH_LABEL = "既存ブラックフェード流用・ストーリー開始整理";
+  window.TENOTSU_BUILD_VERSION = "v039_281";
+  window.TENOTSU_BUILD_LABEL = "ストーリー開始を黒からフェードインへ修正";
+  window.TEN_OTSU_PATCH_VERSION = "v039_281";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー開始を黒からフェードインへ修正";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_280";
-  window.TENOTSU_V039.BUILD_LABEL = "既存ブラックフェード流用・ストーリー開始整理";
+  window.TENOTSU_V039.VERSION = "v039_281";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー開始を黒からフェードインへ修正";
 })();
