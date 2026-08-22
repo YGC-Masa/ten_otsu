1ed553ab8c39f972f7c4f3cefaf4f745f37f93c9
YGC-Masa <osaru355@gmail.com>
2026-08-23 07:57:58 +0900

    v039_285: update runtime version

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_284 */
+/* v039_285 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_284";
-  window.TENOTSU_BUILD_LABEL = "ストーリー終了黒べた塗り解除・事務所起動復帰";
-  window.TEN_OTSU_PATCH_VERSION = "v039_284";
-  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了黒べた塗り解除・事務所起動復帰";
+  window.TENOTSU_BUILD_VERSION = "v039_285";
+  window.TENOTSU_BUILD_LABEL = "ストーリー終了フェード解除をゆっくり化";
+  window.TEN_OTSU_PATCH_VERSION = "v039_285";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了フェード解除をゆっくり化";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_284";
-  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了黒べた塗り解除・事務所起動復帰";
+  window.TENOTSU_V039.VERSION = "v039_285";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了フェード解除をゆっくり化";
 })();
