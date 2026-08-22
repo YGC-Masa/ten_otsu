7d29eb412f098f0ad73d67192773253d22edb3dd
YGC-Masa <osaru355@gmail.com>
2026-08-23 07:35:04 +0900

    v039_284 update runtime version

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_283 */
+/* v039_284 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_283";
-  window.TENOTSU_BUILD_LABEL = "ストーリー終了を事務所起動挙動へ統一";
-  window.TEN_OTSU_PATCH_VERSION = "v039_283";
-  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了を事務所起動挙動へ統一";
+  window.TENOTSU_BUILD_VERSION = "v039_284";
+  window.TENOTSU_BUILD_LABEL = "ストーリー終了黒べた塗り解除・事務所起動復帰";
+  window.TEN_OTSU_PATCH_VERSION = "v039_284";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了黒べた塗り解除・事務所起動復帰";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_283";
-  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了を事務所起動挙動へ統一";
+  window.TENOTSU_V039.VERSION = "v039_284";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了黒べた塗り解除・事務所起動復帰";
 })();
