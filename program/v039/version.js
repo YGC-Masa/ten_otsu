553043f8743541c1667be797e310c52d9475871e
YGC-Masa <osaru355@gmail.com>
2026-08-22 16:02:08 +0900

    chore: bump runtime version to v039_275

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_274 */
+/* v039_275 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_274";
-  window.TENOTSU_BUILD_LABEL = "Wキー黒画面化修正・既定オート維持";
-  window.TEN_OTSU_PATCH_VERSION = "v039_274";
-  window.TEN_OTSU_PATCH_LABEL = "Wキー黒画面化修正・既定オート維持";
+  window.TENOTSU_BUILD_VERSION = "v039_275";
+  window.TENOTSU_BUILD_LABEL = "左下バージョン葉要素のみ非表示・既定オート維持";
+  window.TEN_OTSU_PATCH_VERSION = "v039_275";
+  window.TEN_OTSU_PATCH_LABEL = "左下バージョン葉要素のみ非表示・既定オート維持";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_274";
-  window.TENOTSU_V039.BUILD_LABEL = "Wキー黒画面化修正・既定オート維持";
+  window.TENOTSU_V039.VERSION = "v039_275";
+  window.TENOTSU_V039.BUILD_LABEL = "左下バージョン葉要素のみ非表示・既定オート維持";
 })();
