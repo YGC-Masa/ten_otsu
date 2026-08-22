0ac5adde8ab274045c1a38b3955c3a6289ce3c5c
YGC-Masa <osaru355@gmail.com>
2026-08-23 08:27:39 +0900

    v039_287: version更新

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_286 */
+/* v039_287 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_286";
-  window.TENOTSU_BUILD_LABEL = "ストーリー終了フェード解除1600ms化";
-  window.TEN_OTSU_PATCH_VERSION = "v039_286";
-  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了フェード解除1600ms化";
+  window.TENOTSU_BUILD_VERSION = "v039_287";
+  window.TENOTSU_BUILD_LABEL = "ストーリー終了の黒入りをフェード化";
+  window.TEN_OTSU_PATCH_VERSION = "v039_287";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了の黒入りをフェード化";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_286";
-  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了フェード解除1600ms化";
+  window.TENOTSU_V039.VERSION = "v039_287";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了の黒入りをフェード化";
 })();
