57f87ed5cd65a33dcf7ba6defd28ad31efffb635
YGC-Masa <osaru355@gmail.com>
2026-08-22 16:24:13 +0900

    v039_277: update runtime version

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_276 */
+/* v039_277 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_276";
-  window.TENOTSU_BUILD_LABEL = "事務所ワイド安定化・ストーリー開始3秒黒フェード";
-  window.TEN_OTSU_PATCH_VERSION = "v039_276";
-  window.TEN_OTSU_PATCH_LABEL = "事務所ワイド安定化・ストーリー開始3秒黒フェード";
+  window.TENOTSU_BUILD_VERSION = "v039_277";
+  window.TENOTSU_BUILD_LABEL = "ストーリー開始2秒保持1秒フェード";
+  window.TEN_OTSU_PATCH_VERSION = "v039_277";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー開始2秒保持1秒フェード";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_276";
-  window.TENOTSU_V039.BUILD_LABEL = "事務所ワイド安定化・ストーリー開始3秒黒フェード";
+  window.TENOTSU_V039.VERSION = "v039_277";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー開始2秒保持1秒フェード";
 })();
