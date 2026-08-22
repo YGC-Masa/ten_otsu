dc6f5f71f9dcb1149420a500cf52533bd832754d
YGC-Masa <osaru355@gmail.com>
2026-08-22 09:54:45 +0900

    v039_270: update runtime version

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_269 */
+/* v039_270 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_269";
-  window.TENOTSU_BUILD_LABEL = "ビリビリ名称・WebP互換・メンバー画面復旧";
-  window.TEN_OTSU_PATCH_VERSION = "v039_269";
-  window.TEN_OTSU_PATCH_LABEL = "ビリビリ名称・WebP互換・メンバー画面復旧";
+  window.TENOTSU_BUILD_VERSION = "v039_270";
+  window.TENOTSU_BUILD_LABEL = "ストーリー再生デバッグ・オートプレイ試作";
+  window.TEN_OTSU_PATCH_VERSION = "v039_270";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー再生デバッグ・オートプレイ試作";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_269";
-  window.TENOTSU_V039.BUILD_LABEL = "ビリビリ名称・WebP互換・メンバー画面復旧";
+  window.TENOTSU_V039.VERSION = "v039_270";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー再生デバッグ・オートプレイ試作";
 })();
