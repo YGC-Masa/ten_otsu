957c2826c6e73bd5d3604e20ee530ab1716cf2a5
YGC-Masa <osaru355@gmail.com>
2026-08-22 15:49:08 +0900

    v039_273: update runtime version

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_272 */
+/* v039_273 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_272";
-  window.TENOTSU_BUILD_LABEL = "ストーリーUI切替で左下バージョンも非表示";
-  window.TEN_OTSU_PATCH_VERSION = "v039_272";
-  window.TEN_OTSU_PATCH_LABEL = "ストーリーUI切替で左下バージョンも非表示";
+  window.TENOTSU_BUILD_VERSION = "v039_273";
+  window.TENOTSU_BUILD_LABEL = "ストーリー既定オート・残留バージョン非表示";
+  window.TEN_OTSU_PATCH_VERSION = "v039_273";
+  window.TEN_OTSU_PATCH_LABEL = "ストーリー既定オート・残留バージョン非表示";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_272";
-  window.TENOTSU_V039.BUILD_LABEL = "ストーリーUI切替で左下バージョンも非表示";
+  window.TENOTSU_V039.VERSION = "v039_273";
+  window.TENOTSU_V039.BUILD_LABEL = "ストーリー既定オート・残留バージョン非表示";
 })();
