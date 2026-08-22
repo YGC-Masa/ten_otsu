818171a63a6386406597d1fec8b903ae5e7e81b3
YGC-Masa <osaru355@gmail.com>
2026-08-22 18:38:56 +0900

    chore: update runtime version v039_282

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_281 */
+/* v039_282 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_281";
-  window.TENOTSU_BUILD_LABEL = "ストーリー開始を黒からフェードインへ修正";
-  window.TEN_OTSU_PATCH_VERSION = "v039_281";
-  window.TEN_OTSU_PATCH_LABEL = "ストーリー開始を黒からフェードインへ修正";
+  window.TENOTSU_BUILD_VERSION = "v039_282";
+  window.TENOTSU_BUILD_LABEL = "黒幕最前面化・事務所復帰サーフェス掃除";
+  window.TEN_OTSU_PATCH_VERSION = "v039_282";
+  window.TEN_OTSU_PATCH_LABEL = "黒幕最前面化・事務所復帰サーフェス掃除";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_281";
-  window.TENOTSU_V039.BUILD_LABEL = "ストーリー開始を黒からフェードインへ修正";
+  window.TENOTSU_V039.VERSION = "v039_282";
+  window.TENOTSU_V039.BUILD_LABEL = "黒幕最前面化・事務所復帰サーフェス掃除";
 })();
