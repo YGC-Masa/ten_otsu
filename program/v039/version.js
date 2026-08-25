6acc9515797d6bd73fe97f09c05a3cf7dae498e2
YGC-Masa <osaru355@gmail.com>
2026-08-25 18:34:52 +0900

    v039_289 version.js更新

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_288 */
+/* v039_289 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_288";
-  window.TENOTSU_BUILD_LABEL = "モバイル横メンバーUI調整・チャコール仮追加";
-  window.TEN_OTSU_PATCH_VERSION = "v039_288";
-  window.TEN_OTSU_PATCH_LABEL = "モバイル横メンバーUI調整・チャコール仮追加";
+  window.TENOTSU_BUILD_VERSION = "v039_289";
+  window.TENOTSU_BUILD_LABEL = "チャコール画像をメンバー画面へ反映";
+  window.TEN_OTSU_PATCH_VERSION = "v039_289";
+  window.TEN_OTSU_PATCH_LABEL = "チャコール画像をメンバー画面へ反映";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_288";
-  window.TENOTSU_V039.BUILD_LABEL = "モバイル横メンバーUI調整・チャコール仮追加";
+  window.TENOTSU_V039.VERSION = "v039_289";
+  window.TENOTSU_V039.BUILD_LABEL = "チャコール画像をメンバー画面へ反映";
 })();
