22ecd49f71c15d82c26cc690d25142c48dd0acd2
YGC-Masa <osaru355@gmail.com>
2026-08-25 19:06:00 +0900

    v039_291 version更新

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_289 */
+/* v039_291 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_289";
-  window.TENOTSU_BUILD_LABEL = "チャコール画像をメンバー画面へ反映";
-  window.TEN_OTSU_PATCH_VERSION = "v039_289";
-  window.TEN_OTSU_PATCH_LABEL = "チャコール画像をメンバー画面へ反映";
+  window.TENOTSU_BUILD_VERSION = "v039_291";
+  window.TENOTSU_BUILD_LABEL = "チャコール画像参照をWebP原本へ変更";
+  window.TEN_OTSU_PATCH_VERSION = "v039_291";
+  window.TEN_OTSU_PATCH_LABEL = "チャコール画像参照をWebP原本へ変更";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_289";
-  window.TENOTSU_V039.BUILD_LABEL = "チャコール画像をメンバー画面へ反映";
+  window.TENOTSU_V039.VERSION = "v039_291";
+  window.TENOTSU_V039.BUILD_LABEL = "チャコール画像参照をWebP原本へ変更";
 })();
