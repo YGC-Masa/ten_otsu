a3c15581d933284b834fd94568ca4897ba8e785a
YGC-Masa <osaru355@gmail.com>
2026-08-25 15:44:48 +0900

    v039_288 version js

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_287 */
+/* v039_288 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_287";
-  window.TENOTSU_BUILD_LABEL = "ストーリー終了の黒入りをフェード化";
-  window.TEN_OTSU_PATCH_VERSION = "v039_287";
-  window.TEN_OTSU_PATCH_LABEL = "ストーリー終了の黒入りをフェード化";
+  window.TENOTSU_BUILD_VERSION = "v039_288";
+  window.TENOTSU_BUILD_LABEL = "モバイル横メンバーUI調整・チャコール仮追加";
+  window.TEN_OTSU_PATCH_VERSION = "v039_288";
+  window.TEN_OTSU_PATCH_LABEL = "モバイル横メンバーUI調整・チャコール仮追加";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_287";
-  window.TENOTSU_V039.BUILD_LABEL = "ストーリー終了の黒入りをフェード化";
+  window.TENOTSU_V039.VERSION = "v039_288";
+  window.TENOTSU_V039.BUILD_LABEL = "モバイル横メンバーUI調整・チャコール仮追加";
 })();
