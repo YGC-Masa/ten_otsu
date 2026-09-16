d268cf687c9ed5107a6b84af4bb26bba695a98ae
YGC-Masa <osaru355@gmail.com>
2026-09-17 07:43:56 +0900

    release: v039_294 fix Biribiri sizing and restore version badge

--- program/v039/version.js
+++ program/v039/version.js
@@ -1,11 +1,11 @@
-/* v039_293 */
+/* v039_294 */
 (function(){
   "use strict";
-  window.TENOTSU_BUILD_VERSION = "v039_293";
-  window.TENOTSU_BUILD_LABEL = "オートプレイ秒数設定を追加";
-  window.TEN_OTSU_PATCH_VERSION = "v039_293";
-  window.TEN_OTSU_PATCH_LABEL = "オートプレイ秒数設定を追加";
+  window.TENOTSU_BUILD_VERSION = "v039_294";
+  window.TENOTSU_BUILD_LABEL = "ビリビリ立ち絵サイズ修正・事務所版数常時表示";
+  window.TEN_OTSU_PATCH_VERSION = "v039_294";
+  window.TEN_OTSU_PATCH_LABEL = "ビリビリ立ち絵サイズ修正・事務所版数常時表示";
   window.TENOTSU_V039 = window.TENOTSU_V039 || {};
-  window.TENOTSU_V039.VERSION = "v039_293";
-  window.TENOTSU_V039.BUILD_LABEL = "オートプレイ秒数設定を追加";
+  window.TENOTSU_V039.VERSION = "v039_294";
+  window.TENOTSU_V039.BUILD_LABEL = "ビリビリ立ち絵サイズ修正・事務所版数常時表示";
 })();
